e756cb57dcbf52864d3bc0058b81eda7abac846e
Karl Eklund <localpart@gmail.com>
2013-10-28 18:01:17 +0100

    Adding some things to do in the task

--- init.rb
+++ init.rb
@@ -8,6 +8,21 @@ class Heroku::Command::Release < Heroku::Command::Base
   # Release.
   #
   def index
-    puts "Test"
+    switch_to_master
+    warn_for_schema_diff
+    show_changes
+    approve_changes
+    enter_summary
+    push_to_heroku
+  end
+
+  private
+
+  def switch_to_master
+    `git rev-parse --abbref-ref HEAD` == 'master' or exit("You must switch to master")
+  end
+
+  def warn_for_schema_diff
+    puts `diff --suppress-common-lines -y <(heroku run cat db/schema.rb -r production) db/schema.rb`
   end
 end
